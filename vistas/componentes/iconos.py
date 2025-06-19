0af91a754f57d729aefec55cd349a7a7393ddbca
Yael <srjean21@gmail.com>
2025-06-19 17:17:05 -0400

    (changes) Se cambió el ícono de añadir

--- vistas/componentes/iconos.py
+++ vistas/componentes/iconos.py
@@ -146,17 +146,13 @@ class Iconos:
     @component
     def Añadir(p: VdomDict = {}):  # type: ignore
         return html.svg(
-            {
-                "xmlns": "http://www.w3.org/2000/svg",
-                "viewBox": "0 0 24 24",
-                "fill": "none",
-                "stroke-width": "2",
-                "stroke-linecap": "round",
-                "stroke-linejoin": "round",
-            },
-            svg.circle({"cx": "12", "cy": "12", "r": "10"}),
-            svg.path({"d": "M8 12h8"}),
-            svg.path({"d": "M12 8v8"}),
+            {"viewBox": "0 0 24 24"},
+            svg.path({"d": "M0 0h24v24H0z", "fill": "none"}),
+            svg.path(
+                {
+                    "d": "M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-1-11H7v2h4v4h2v-4h4v-2h-4V7h-2v4z"
+                }
+            ),
         )
 
     @component
